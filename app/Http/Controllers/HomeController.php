7739da2f1c70fa6aded42bda2d9fcd5f5d71778c
Pens <pensnaku@gmail.com>
2017-08-20 01:38:58 +0100

    laravel folder structure updated

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * @param $request
+     * @return null 
+    */
+    public function index(Request $request){
+        return null;
+    }
+}
